40c0a537395b61e796292a8a1121955ce92d9eca
Olaniyi Osoba <stpaulosoba@gmail.com>
2026-05-24 23:46:49 +0100

    Just tested out some css properties and new fonts

---
+++ python.py
@@ -0,0 +1,35 @@
+import sqlite3
+from flask import Flask
+
+app = (__name__)
+
+print("Flask works")
+
+conn = sqlite3.connect("aircrafts.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Aircraft (
+  Registration TEXT PRIMARY KEY,
+
+  Manufacturer TEXT,
+
+  Model TEXT,
+
+  Engine Type TEXT,
+
+  Status TEXT)
+""")
+
+
+
+cursor.execute("""
+ INSERT INTO Aircraft
+ VALUES( "G-NEYO", "Boeing", "B787-9","Rolls-Royce Trent 1000", "Grounded")
+
+""")
+
+
+conn.commit()
+conn.close()
