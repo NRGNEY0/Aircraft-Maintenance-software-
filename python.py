c396531a3bf84e2e439a1cec0fdc8825cd4ce157
Olaniyi Osoba <stpaulosoba@gmail.com>
2026-06-15 22:43:22 +0100

    Able to add new aircrafts to the data base, started work on the maintenance page

--- python.py
+++ python.py
@@ -4,54 +4,79 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 print("Flask works")
+def generateTaskID(): #Function that generates task id
+  with sqlite3.connect("aircrafts.db") as conn:
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT MaintenanceID FROM Maintenance ORDER BY rowid DESC LIMIT 1")
+    last= cursor.fetchone()
+
+    if last is None:
+      return "MX-0001"
+    
+    last_num = int(last[0].split("-")[1])
+    new_num = last_num + 1
+    return f"MX-{new_num:04d}"
+
 
 @app.route("/") #This route is for the home page 
 def home():
 
-  conn = sqlite3.connect("aircrafts.db")
+  with sqlite3.connect("aircrafts.db") as conn:
 
-  cursor = conn.cursor()
+    cursor = conn.cursor()
 
-  cursor.execute("SELECT COUNT(*) FROM Aircraft")
-  total_aircraft = cursor.fetchone()[0]
+    cursor.execute("SELECT COUNT(*) FROM Aircraft")
+    total_aircraft = cursor.fetchone()[0]
 
-  cursor.execute("SELECT COUNT(*) FROM Aircraft WHERE Status ='Grounded'")
-  grounded_Aircraft = cursor.fetchone()[0]
+    cursor.execute("SELECT COUNT(*) FROM Aircraft WHERE Status ='Grounded'")
+    grounded_Aircraft = cursor.fetchone()[0]
 
 
-  
-  
-  conn.close()
-  return render_template(
-    "home.htm",
-    total_aircraft=total_aircraft,
-    grounded_Aircraft=grounded_Aircraft
-  )
+    
+    
+    
+    return render_template(
+      "home.htm",
+      total_aircraft=total_aircraft,
+      grounded_Aircraft=grounded_Aircraft
+    )
 
 
 @app.route("/aircraft")
 def aircraft():
 
-  conn = sqlite3.connect("aircrafts.db")
-  conn.row_factory = sqlite3.Row
-  cursor = conn.cursor()
+  with sqlite3.connect("aircrafts.db") as conn:
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
 
-  cursor.execute("SELECT * FROM Aircraft")
-  Aircrafts = cursor.fetchall()
-  
+    cursor.execute("SELECT * FROM Aircraft")
+    Aircrafts = cursor.fetchall()
+    
 
-  conn.close()
+    
 
 
-  return render_template("aircraft.htm", Aircrafts=Aircrafts)
+    return render_template("aircraft.htm", Aircrafts=Aircrafts)
 
 
 @app.route("/maintenance")
 def maintenance():
-  return render_template("maintenance.htm")
+  with sqlite3.connect("aircrafts.db") as conn:
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Maintenance")
+    Tasks = cursor.fetchall()
 
 
-@app.route("/add aircraft", methods=["GET", "POST"])
+    return render_template("maintenance.htm", Tasks=Tasks)
+
+
+  
+
+
+@app.route("/add_aircraft", methods=["GET", "POST"])
 def add_aircraft():
   if request.method == "POST":
     registration = request.form["Registration"]
@@ -60,16 +85,21 @@ def add_aircraft():
     engine = request.form['Engine']
     status = request.form['Status']
 
-    conn = sqlite3.connect("aircrafts.db")
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO Aircraft VALUES(?, ?, ?, ?, ?)",
-    (registration, manufacturer, model, engine, status))
-    conn.commit()
-    conn.close()
+    with sqlite3.connect("aircrafts.db") as conn:
+      cursor = conn.cursor()
+      cursor.execute("INSERT INTO Aircraft VALUES(?, ?, ?, ?, ?)",
+      (registration, manufacturer, model, engine, status))
+      conn.commit()
+      
 
-    return redirect(url_for("aircraft.htm"))
+      return redirect(url_for("aircraft"))
 
-  return render_template("add aircraft.htm")
+  return render_template("add_aircraft.htm")
+
+@app.route("/add_task" methods=['GET', 'POST'])
+def add_task
+
+
 
 
 
